f35d78ffba31c4d2c299420ab5956a59c064f4bf
HenriqueMAP <henrique.map@outlook.com>
2022-07-15 01:53:33 -0300

    Lecture 261 - Implementation of the class that represents the field as a button.

---
+++ src/br/com/otogamidev/minesweeper/view/GameBoardButton.java
@@ -0,0 +1,12 @@
+package br.com.otogamidev.minesweeper.view;
+
+import br.com.otogamidev.minesweeper.model.BoardField;
+
+import javax.swing.*;
+
+@SuppressWarnings("serial")
+public class GameBoardButton extends JButton {
+    public GameBoardButton(final BoardField boardField) {
+
+    }
+}
